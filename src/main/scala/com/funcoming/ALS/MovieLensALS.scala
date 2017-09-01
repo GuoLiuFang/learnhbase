43d832453bfc7aac16e46f7599f8ded8950531c5
GuoLiuFang <guoliufangopen@163.com>
2017-09-01 17:12:27 +0800

    研究完毕，参数选项处理，非常有借鉴意义

--- src/main/scala/com/funcoming/ALS/MovieLensALS.scala
+++ src/main/scala/com/funcoming/ALS/MovieLensALS.scala
@@ -55,128 +55,131 @@ object MovieLensALS {
     val defaultParams = Params()
     println(defaultParams.toString())
 
-    //    val parser = new OptionParser[Params]("MovieLensALS") {
-    //      head("MovieLensALS: an example app for ALS on MovieLens data.")
-    //      opt[Int]("rank")
-    //        .text(s"rank, default: ${defaultParams.rank}")
-    //        .action((x, c) => c.copy(rank = x))
-    //      opt[Int]("numIterations")
-    //        .text(s"number of iterations, default: ${defaultParams.numIterations}")
-    //        .action((x, c) => c.copy(numIterations = x))
-    //      opt[Double]("lambda")
-    //        .text(s"lambda (smoothing constant), default: ${defaultParams.lambda}")
-    //        .action((x, c) => c.copy(lambda = x))
-    //      opt[Unit]("kryo")
-    //        .text("use Kryo serialization")
-    //        .action((_, c) => c.copy(kryo = true))
-    //      opt[Int]("numUserBlocks")
-    //        .text(s"number of user blocks, default: ${defaultParams.numUserBlocks} (auto)")
-    //        .action((x, c) => c.copy(numUserBlocks = x))
-    //      opt[Int]("numProductBlocks")
-    //        .text(s"number of product blocks, default: ${defaultParams.numProductBlocks} (auto)")
-    //        .action((x, c) => c.copy(numProductBlocks = x))
-    //      opt[Unit]("implicitPrefs")
-    //        .text("use implicit preference")
-    //        .action((_, c) => c.copy(implicitPrefs = true))
-    //      arg[String]("<input>")
-    //        .required()
-    //        .text("input paths to a MovieLens dataset of ratings")
-    //        .action((x, c) => c.copy(input = x))
-    //      note(
-    //        """
-    //          |For example, the following command runs this app on a synthetic dataset:
-    //          |
-    //          | bin/spark-submit --class org.apache.spark.examples.mllib.MovieLensALS \
-    //          |  examples/target/scala-*/spark-examples-*.jar \
-    //          |  --rank 5 --numIterations 20 --lambda 1.0 --kryo \
-    //          |  data/mllib/sample_movielens_data.txt
-    //        """.stripMargin)
-    //    }
-    //
-    //    parser.parse(args, defaultParams).map { params =>
-    //      run(params)
-    //    } getOrElse {
-    //      System.exit(1)
-    //    }
+        val parser = new OptionParser[Params]("MovieLensALS") {
+          head("MovieLensALS: an example app for ALS on MovieLens data.")
+          opt[Int]("rank")
+            .text(s"rank, default: ${defaultParams.rank}")
+            .action((x, c) => c.copy(rank = x))
+//          c是 Params。。。这里定义映射关系。。。rank 是 c 的一个成员变量。。。x 是获取到的值
+          opt[Int]("numIterations")
+            .text(s"number of iterations, default: ${defaultParams.numIterations}")
+            .action((x, c) => c.copy(numIterations = x))
+          opt[Double]("lambda")
+            .text(s"lambda (smoothing constant), default: ${defaultParams.lambda}")
+            .action((x, c) => c.copy(lambda = x))
+          opt[Unit]("kryo")
+            .text("use Kryo serialization")
+            .action((_, c) => c.copy(kryo = true))
+//          类似 flag 不需要要值。。是开关选项。。。
+          opt[Int]("numUserBlocks")
+            .text(s"number of user blocks, default: ${defaultParams.numUserBlocks} (auto)")
+            .action((x, c) => c.copy(numUserBlocks = x))
+          opt[Int]("numProductBlocks")
+            .text(s"number of product blocks, default: ${defaultParams.numProductBlocks} (auto)")
+            .action((x, c) => c.copy(numProductBlocks = x))
+          opt[Unit]("implicitPrefs")
+            .text("use implicit preference")
+            .action((_, c) => c.copy(implicitPrefs = true))
+          arg[String]("<input>")
+            .required()
+            .text("input paths to a MovieLens dataset of ratings")
+            .action((x, c) => c.copy(input = x))
+          note(
+            """
+              |For example, the following command runs this app on a synthetic dataset:
+              |
+              | bin/spark-submit --class org.apache.spark.examples.mllib.MovieLensALS \
+              |  examples/target/scala-*/spark-examples-*.jar \
+              |  --rank 5 --numIterations 20 --lambda 1.0 --kryo \
+              |  data/mllib/sample_movielens_data.txt
+            """.stripMargin)
+        }
+//在这里进行的解析。。。
+        parser.parse(args, defaultParams).map { params =>
+          run(params)
+        } getOrElse {
+          System.exit(1)
+        }
   }
 
-//  def run(params: Params) {
-//    val conf = new SparkConf().setAppName(s"MovieLensALS with $params")
-//    if (params.kryo) {
-//      conf.registerKryoClasses(Array(classOf[mutable.BitSet], classOf[Rating]))
-//        .set("spark.kryoserializer.buffer", "8m")
-//    }
-//    val sc = new SparkContext(conf)
-//
-//    Logger.getRootLogger.setLevel(Level.WARN)
-//
-//    val implicitPrefs = params.implicitPrefs
-//
-//    val ratings = sc.textFile(params.input).map { line =>
-//      val fields = line.split("::")
-//      if (implicitPrefs) {
-//        /*
-//         * MovieLens ratings are on a scale of 1-5:
-//         * 5: Must see
-//         * 4: Will enjoy
-//         * 3: It's okay
-//         * 2: Fairly bad
-//         * 1: Awful
-//         * So we should not recommend a movie if the predicted rating is less than 3.
-//         * To map ratings to confidence scores, we use
-//         * 5 -> 2.5, 4 -> 1.5, 3 -> 0.5, 2 -> -0.5, 1 -> -1.5. This mappings means unobserved
-//         * entries are generally between It's okay and Fairly bad.
-//         * The semantics of 0 in this expanded world of non-positive weights
-//         * are "the same as never having interacted at all".
-//         */
-//        Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble - 2.5)
-//      } else {
-//        Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble)
-//      }
-//    }.cache()
-//
-//    val numRatings = ratings.count()
-//    val numUsers = ratings.map(_.user).distinct().count()
-//    val numMovies = ratings.map(_.product).distinct().count()
-//
-//    println(s"Got $numRatings ratings from $numUsers users on $numMovies movies.")
-//
-//    val splits = ratings.randomSplit(Array(0.8, 0.2))
-//    val training = splits(0).cache()
-//    val test = if (params.implicitPrefs) {
-//      /*
-//       * 0 means "don't know" and positive values mean "confident that the prediction should be 1".
-//       * Negative values means "confident that the prediction should be 0".
-//       * We have in this case used some kind of weighted RMSE. The weight is the absolute value of
-//       * the confidence. The error is the difference between prediction and either 1 or 0,
-//       * depending on whether r is positive or negative.
-//       */
-//      splits(1).map(x => Rating(x.user, x.product, if (x.rating > 0) 1.0 else 0.0))
-//    } else {
-//      splits(1)
-//    }.cache()
-//
-//    val numTraining = training.count()
-//    val numTest = test.count()
-//    println(s"Training: $numTraining, test: $numTest.")
-//
-//    ratings.unpersist(blocking = false)
-//
-//    val model = new ALS()
-//      .setRank(params.rank)
-//      .setIterations(params.numIterations)
-//      .setLambda(params.lambda)
-//      .setImplicitPrefs(params.implicitPrefs)
-//      .setUserBlocks(params.numUserBlocks)
-//      .setProductBlocks(params.numProductBlocks)
-//      .run(training)
-//
-//    val rmse = computeRmse(model, test, params.implicitPrefs)
-//
-//    println(s"Test RMSE = $rmse.")
-//
-//    sc.stop()
-//  }
+  def run(params: Params) {
+//    参数处理完毕。。。整理后的值，进入程序..
+    val conf = new SparkConf().setAppName(s"MovieLensALS with $params")
+    if (params.kryo) {
+      conf.registerKryoClasses(Array(classOf[mutable.BitSet], classOf[Rating]))
+        .set("spark.kryoserializer.buffer", "8m")
+    }
+    val sc = new SparkContext(conf)
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val implicitPrefs = params.implicitPrefs
+
+    val ratings = sc.textFile(params.input).map { line =>
+      val fields = line.split("::")
+      if (implicitPrefs) {
+        /*
+         * MovieLens ratings are on a scale of 1-5:
+         * 5: Must see
+         * 4: Will enjoy
+         * 3: It's okay
+         * 2: Fairly bad
+         * 1: Awful
+         * So we should not recommend a movie if the predicted rating is less than 3.
+         * To map ratings to confidence scores, we use
+         * 5 -> 2.5, 4 -> 1.5, 3 -> 0.5, 2 -> -0.5, 1 -> -1.5. This mappings means unobserved
+         * entries are generally between It's okay and Fairly bad.
+         * The semantics of 0 in this expanded world of non-positive weights
+         * are "the same as never having interacted at all".
+         */
+        Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble - 2.5)
+      } else {
+        Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble)
+      }
+    }.cache()
+
+    val numRatings = ratings.count()
+    val numUsers = ratings.map(_.user).distinct().count()
+    val numMovies = ratings.map(_.product).distinct().count()
+
+    println(s"Got $numRatings ratings from $numUsers users on $numMovies movies.")
+
+    val splits = ratings.randomSplit(Array(0.8, 0.2))
+    val training = splits(0).cache()
+    val test = if (params.implicitPrefs) {
+      /*
+       * 0 means "don't know" and positive values mean "confident that the prediction should be 1".
+       * Negative values means "confident that the prediction should be 0".
+       * We have in this case used some kind of weighted RMSE. The weight is the absolute value of
+       * the confidence. The error is the difference between prediction and either 1 or 0,
+       * depending on whether r is positive or negative.
+       */
+      splits(1).map(x => Rating(x.user, x.product, if (x.rating > 0) 1.0 else 0.0))
+    } else {
+      splits(1)
+    }.cache()
+
+    val numTraining = training.count()
+    val numTest = test.count()
+    println(s"Training: $numTraining, test: $numTest.")
+
+    ratings.unpersist(blocking = false)
+
+    val model = new ALS()
+      .setRank(params.rank)
+      .setIterations(params.numIterations)
+      .setLambda(params.lambda)
+      .setImplicitPrefs(params.implicitPrefs)
+      .setUserBlocks(params.numUserBlocks)
+      .setProductBlocks(params.numProductBlocks)
+      .run(training)
+
+    val rmse = computeRmse(model, test, params.implicitPrefs)
+
+    println(s"Test RMSE = $rmse.")
+
+    sc.stop()
+  }
 
   /** Compute RMSE (Root Mean Squared Error). */
 //  def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating], implicitPrefs: Boolean)
--- src/main/scala/com/funcoming/test/testReflect.scala
+++ src/main/scala/com/funcoming/test/testReflect.scala
@@ -9,7 +9,24 @@ abstract class testReflect[T: TypeTag] {
   override def toString: String = {
     val tpe = tag.tpe
     println("------测试方法调用------" + tpe)
-    ""
+    //    collect按照条件进行过滤
+    //    a field of a case class
+    val allAccessors = tpe.declarations.collect {
+      case m: MethodSymbol if m.isCaseAccessor => m
+    }
+    allAccessors foreach println
+    println("-----查看区别-----")
+    println(tpe.declarations)
+    val mirror = runtimeMirror(getClass.getClassLoader)
+    val instanceMirror = mirror.reflect(this)
+    allAccessors.map {
+      f =>
+        val paramName = f.name.toString
+        val fieldMirror = instanceMirror.reflectField(f)
+        val paramValue = fieldMirror.get
+        s"  $paramName:\t$paramValue"
+
+    }.mkString("{\n", ",\n", "\n}")
   }
 
 }
